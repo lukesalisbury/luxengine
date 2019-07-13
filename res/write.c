61901c5b4f932f659646e568e318bde01c54d12c
Luke Salisbury <dev@lukesalisbury.name>
2019-07-13 12:33:54 +1000

    Update write.c

--- res/write.c
+++ res/write.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+
+void elix_cstring_sanitise( char * string ) {
+	size_t pos = 0;
+	size_t length = strlen(string);
+	while( string[pos] != 0 ) {
+		if ( string[pos] == '\\' && string[pos+1] == '$' ) {
+			memmove(&string[pos], &string[pos+1], length);
+		}
+		pos++;
+		length--;
+	}
+}
 
 int main(int argc, char *argv[]) {
-	if ( argc > 1)
+	if ( argc > 1) {
+		elix_cstring_sanitise(argv[1]);
 		printf("%s\n", argv[1]);
+	}
 	return 0;
 }
